0a1b255023fec8e9a774cd9e133ccd9a2b957b8a
MinoTianaR <s.mino.tiana@gmail.com>
2023-01-11 15:19:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
-app.listen(3000, () => console.log('Server started'))
+app.listen(3000, () => console.log('Server started'));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:3000/mydb', {useNewUrlParser: true});
